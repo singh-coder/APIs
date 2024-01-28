9534013b7ca6fd449e71eada3b433859a1f8d8bf
ashutosh <ashutosh.aryan92@gmail.com>
2024-01-28 20:51:57 +0530

    Added DB Context logic and related dependencies

--- HealthCareMate/Presentation/HealthCareMate.API/Program.cs
+++ HealthCareMate/Presentation/HealthCareMate.API/Program.cs
@@ -1,7 +1,9 @@
+using HealthCareMate.Persistence.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddPersistenceLayer(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
